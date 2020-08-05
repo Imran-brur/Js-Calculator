993e5f669c8dd9819c8671a2ec029e0e2e649fd9
Imran-brur <60210631+Imran-brur@users.noreply.github.com>
2020-08-05 21:50:52 +0600

    calculator done

---
+++ script.js
@@ -0,0 +1,11 @@
+ class calculator {
+      
+ }
+ 
+ const numberButton = document.querySelectorAll('[data-number]')
+ const operationButton = document.querySelectorAll('[data-operation]')
+ const equalsButton = document.querySelectorAll('[data-equals]')
+ const deleteButton = document.querySelectorAll('[data-delete]')
+ const allClearButton = document.querySelectorAll('[data-all-clear]')
+ const previousOperandButton = document.querySelectorAll('[data-previous-operand]')
+ const currentOperandButton = document.querySelectorAll('[data-current-operand]')
